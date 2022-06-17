4fd6760dd2dd9105d92bd5674228b4203578619e
ghxm <max.haag@gmx.net>
2022-06-17 16:48:37 +0200

    fix silence detection

--- chuntfm.py
+++ chuntfm.py
@@ -2,7 +2,7 @@ import asyncio
 import aiohttp
 from io import BytesIO
 from pydub import AudioSegment
-from pydub.silence import detect_silence
+from pydub.silence import detect_silence, detect_nonsilent
 
 
 
@@ -29,12 +29,18 @@ async def chuntfm_live():
 
     sound = AudioSegment.from_mp3(recording)
 
-    silence_check = detect_silence(sound, min_silence_len=900)
+    silence_check = detect_silence(sound, min_silence_len=900, silence_thresh=-30)
 
 
     # if all detected silences ar elonger than 900 miliseconds
     if all([len(range(silence[0], silence[1]))>900 for silence in silence_check]):
-        return True
-    else:
         return False
+    else:
+        return True
 
+
+tasks = [chuntfm_live() for x in range(1)]
+loop = asyncio.get_event_loop()
+loop.set_debug(True)
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
