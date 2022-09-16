af56573d884bf390d31f579f3193fa6c849bdf27
roniqueh <roniquehossain@gmail.com>
2022-09-16 15:54:16 +0100

    chunted fortunes

--- wombot.py
+++ wombot.py
@@ -20,6 +20,15 @@ import html
 from urllib.parse import urlparse
 import bs4
 import struct
+import nltk
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nlty.download('punkt')
+try:
+    nltk.data.find('taggers/averaged_perceptron_tagger')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger')
 
 import radioactivity
 import schedule
@@ -1024,6 +1033,39 @@ class MyBot(chatango.Client):
                     + message.user.showname
                     + " : "
                     + (random.choice(fortunes.fortunecookie))
+                    .replace(".", "")
+                    .lower()
+                )
+
+            elif cmd == "cfortune":
+                await message.room.delete_message(message)
+                sentence = random.choice(fortunes.fortunecookie)
+                tokens = nltk.word_tokenize(sentence)
+                tagged = [[token, tag] for (token, tag) in nltk.pos_tag(tokens)]
+                nn_idx = []
+                nns_idx = []
+                for i, [token, tag] in enumerate(tagged):
+                    if tag == 'VBD':
+                        tagged[i][0] = 'chunted'
+                    elif tag == 'NN':
+                        nn_idx.append(i)
+                    elif tag == 'NNS':
+                        nns_idx.append(i)
+                try:
+                    tagged[random.choice(nn_idx)][0] = 'chunt'
+                except IndexError:
+                    pass
+                try:
+                    tagged[random.choice(nns_idx)][0] = 'chunts'
+                except IndexError:
+                    pass
+                cfortune = ' '.join([token[0] for token in tagged])
+                print(cfortune)
+                await message.channel.send(
+                    "your chunted fortune, "
+                    + message.user.showname
+                    + " : "
+                    + cfortune
                     .replace(".", "")
                     .lower()
                 )
