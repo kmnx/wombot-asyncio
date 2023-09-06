451b911a72e4dd4862ad90c22f6f7b22392cd7dd
knm4x <max.hampel@gmail.com>
2023-09-06 22:58:41 +0200

    id history insert partially working

---
+++ aiosqliteclass_id.py
@@ -0,0 +1,128 @@
+import asyncio
+import aiosqlite
+import logging as LOGGER
+
+
+async def create_conn():
+    db = Sqlite3Class()
+    await db._init()
+
+    return db
+
+
+class Sqlite3Class:
+    # async def __init__(self, loop):
+    # pass
+    # self.conn = await aiosqlite.connect("pythonsqlite.db", loop=loop)
+    # self.cursor = await self.conn.cursor()
+    # self.cursor.row_factory = lambda cursor, row: row[0]
+
+    async def _init(self):
+        self.conn = await aiosqlite.connect("db_idhistory.db")
+        # self.conn.row_factory = lambda cursor, row: row[0]
+        # self.conn.row_factory = aiosqlite.Row
+        self.cursor = await self.conn.cursor()
+        print("init aiosqliteclass done ")
+
+    async def _close():
+        await self.conn.close()
+
+    async def insert_id_request(
+        self,
+        timestamp_utc,
+        username,
+        request_source,
+        request_command,
+        station,
+        show_name,
+        shazam_full,
+        artist,
+        title,
+        bandcamp,
+        verified,
+    ):
+        await self.cursor.execute(
+            "INSERT INTO chuntfm (timestamp_utc, username, request_source, request_command, station, show_name, shazam_full, artist, title, bandcamp, verified) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+            [
+                str(timestamp_utc),
+                username,
+                request_source,
+                request_command,
+                station,
+                show_name,
+                str(shazam_full),
+                artist,
+                title,
+                bandcamp,
+                verified,
+            ],
+        )
+        await self.conn.commit()
+        # result_tag_id = self.cursor.lastrowid
+        # return result_tag_id
+        print("inserted id request")
+
+    async def query_history_all(self):
+        #  returns all history
+        await self.cursor.execute("SELECT * FROM chuntfm")
+        result = await self.cursor.fetchall()
+        if result:
+            return result
+
+        else:
+            return None
+
+    async def query_history_user(self, username):
+        #  returns all history
+        await self.cursor.execute("SELECT * FROM chuntfm WHERE username=? ", [username])
+        result = await self.cursor.fetchall()
+        if result:
+            return result
+
+        else:
+            return None
+
+
+async def run(loop, query_in):
+    db = await create_conn()
+    await db.insert_id_request(*query_in)
+    r = await db.query_history_all()
+    if r:
+        for result in r:
+            print(result)
+    """
+    async with conn.cursor() as cur:
+        await cur.execute("SELECT * FROM tag_table WHERE tag_name=? ", ['woi',])
+        r = await cur.fetchall()
+        print(r)
+    """
+    await db.conn.close()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+
+    # query_in = input("enter search term: ")
+    # url_in = input("enter url: ")
+    query_in = [
+        "2023-09-06T15:02:26Z",
+        "knmx",
+        "chuntoo",
+        "id1",
+        "chuntfm",
+        "anon livestream",
+        "shazam says bla",
+        "unknown artists",
+        "good music",
+        "http://bla",
+        None,
+    ]
+    # task = asyncio.gather(run())
+    try:
+        loop.run_until_complete(run(loop, query_in))
+        # loop.run_forever()
+    except KeyboardInterrupt:
+        print("[KeyboardInterrupt] Killed bot.")
+    finally:
+        # task.cancel()
+        loop.close()
---
+++ sqlite_create_idhistory_db.py
@@ -0,0 +1,33 @@
+import sqlite3
+from sqlite3 import Error
+from pathlib import Path
+import os.path
+
+
+def create_connection(db_file):
+    """create a database connection to a SQLite database"""
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def run():
+    conn_obj = create_connection("db_idhistory.db")
+    cursor_obj = conn_obj.cursor()
+    cursor_obj.execute("DROP TABLE IF EXISTS chuntfm")
+    cursor_obj.execute(
+        "CREATE TABLE IF NOT EXISTS chuntfm (id INTEGER PRIMARY KEY, timestamp_utc, username, request_source, request_command, station, show_name, shazam_full, artist, title, bandcamp, verified);"
+    )
+
+    conn_obj.commit()
+    cursor_obj.close()
+    print("Database created successfully")
+
+
+if __name__ == "__main__":
+    run()
