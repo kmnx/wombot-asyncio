a9b2078304ad237fd8ab3939b07d728296bd3ca2
knm4x <max.hampel@gmail.com>
2023-09-08 13:37:23 +0200

    fixed goth to not posted every minute

--- wombot.py
+++ wombot.py
@@ -235,8 +235,8 @@ async def schedule_gif_of_the_hour():
 
     # cron_min = aiocron.crontab('*/1 * * * *', func=post_gif_of_the_hour, args=("At every minute",), start=True)
     cron_jub = aiocron.crontab(
-        # "0 */1 * * *",
-        "*/1 * * * *",
+        "0 */1 * * *",
+        # "*/1 * * * *",
         func=post_gif_of_the_hour,
         args=("At minute 0 past every hour.",),
         start=True,
