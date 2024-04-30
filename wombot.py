fa0b37eec8d3806327830ba9fa2a7523c3ced5eb
kmnx <max.hampel@gmail.com>
2024-04-30 14:22:38 +0100

    Change output message on whenis when no show found

--- wombot.py
+++ wombot.py
@@ -1445,7 +1445,7 @@ class MyBot(chatango.Client):
                                         )
                                         break
                                     else:
-                                        chuntfm_queried_show = "No shows found with query: " + args
+                                        chuntfm_queried_show = "No upcoming shows found with query: " + args
                             else:
                                 chuntfm_queried_show = "i think chunt.org might be broken"
                     except Exception as e:
