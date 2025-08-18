4805da71cf39767ae2b988495a0222dcfc55e6ed
ghxm <max.haag@gmx.net>
2025-08-18 08:33:27 +0200

    Fix help message assignemnt

--- cmd/help.py
+++ cmd/help.py
@@ -11,21 +11,19 @@ async def help_handler(self, message, cmd, args):
 
     # TODO make this dynamically output all registered commands
 
-    import wombot
-    help_message = help_message
-    
+    help_message = (
+            "commands: \r \r "
+            + "!id1 to shazam chunt1 \r!idnts1 for NTS1 \r!idyourfavouritestation for your favourite station \r \r"
+            + "!fortune (your daily fortune)  \r \r "
+            + "!shoutout [username]  \r "
+            + "!b2b for some random random gifs \r !rnd for even more random gifs \r"
+            + "gifs curated by bigbunnybrer, oscmal, and others \r \r"
+            + "keep chuntin!"
+    )
+
     await message.channel.send(help_message)
     await message.room.client.pm.send_message(message.user, help_message)
 
 
 # Register the command
 register_exact("help", ["help"], help_handler)
-help_message = (
-    "commands: \r \r "
-    + "!id1 to shazam chunt1 \r!idnts1 for NTS1 \r!idyourfavouritestation for your favourite station \r \r"
-    + "!fortune (your daily fortune)  \r \r "
-    + "!shoutout [username]  \r "
-    + "!b2b for some random random gifs \r !rnd for even more random gifs \r"
-    + "gifs curated by bigbunnybrer, oscmal, and others \r \r"
-    + "keep chuntin!"
-)
